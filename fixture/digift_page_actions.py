8fc2621cb5ee08bdc448ab5f6ded86f474aa2221
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-22 18:46:55 +0300

    Added click_nominal_button methods

--- fixture/digift_page_actions.py
+++ fixture/digift_page_actions.py
@@ -12,4 +12,26 @@ class DigiftPageHelper:
         wd.execute_script("return arguments[0].scrollIntoView(true);", wd.find_element(*Locators.CARD_NOMINAL_VALUE_TEXT))
         time.sleep(2)
 
+    def click_nominal_500(self):
+        wd = self.app.wd
+        wd.find_element(*Locators.NOMINAL_500).click()
 
+    def click_nominal_1000(self):
+        wd = self.app.wd
+        wd.find_element(*Locators.NOMINAL_1000).click()
+
+    def click_nominal_2000(self):
+        wd = self.app.wd
+        wd.find_element(*Locators.NOMINAL_2000).click()
+
+    def click_nominal_3000(self):
+        wd = self.app.wd
+        wd.find_element(*Locators.NOMINAL_3000).click()
+
+    def click_nominal_5000(self):
+        wd = self.app.wd
+        wd.find_element(*Locators.NOMINAL_5000).click()
+
+    def click_nominal_10000(self):
+        wd = self.app.wd
+        wd.find_element(*Locators.NOMINAL_10000).click()
