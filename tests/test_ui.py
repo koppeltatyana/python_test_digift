abed7392e45eb2240bec2b34b46cad9948fe8bd5
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-23 09:59:14 +0300

    Added Allure decorators in test

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,8 +1,16 @@
+import allure
+
+
 def test_ui(app):
     nominal_value_list = [500, 1000, 2000, 3000, 5000, 10000]
-    app.open_home_page()
-    app.ui_actions.scroll_page_down_till_nominal()
+    with allure.step("Open home page"):
+        app.open_home_page()
+    with allure.step("Scroll home page till nominal block"):
+        app.ui_actions.scroll_page_down_till_nominal()
     for nominal_value in nominal_value_list:
-        app.ui_actions.click_nominal(nominal_value)
-        assert app.ui_actions.check_enable_nominal_button(nominal_value)
-        assert app.ui_actions.check_identity_nominal(nominal_value)
+        with allure.step("When I click the {} nominal value button".format(nominal_value)):
+            app.ui_actions.click_nominal(nominal_value)
+        with allure.step("Then that button becomes enable"):
+            assert app.ui_actions.check_enable_nominal_button(nominal_value)
+        with allure.step("And then input field has chosen nominal value"):
+            assert app.ui_actions.check_identity_nominal(nominal_value)
