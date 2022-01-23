8e44079104ea27bf6f441e66175b37ba68832d56
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-23 09:41:56 +0300

    Edited ui test

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,4 +1,8 @@
 def test_ui(app):
+    nominal_value_list = [500, 1000, 2000, 3000, 5000, 10000]
     app.open_home_page()
     app.ui_actions.scroll_page_down_till_nominal()
-    app.ui_actions.click_nominal_500()
+    for nominal_value in nominal_value_list:
+        app.ui_actions.click_nominal(nominal_value)
+        assert app.ui_actions.check_enable_nominal_button(nominal_value)
+        assert app.ui_actions.check_identity_nominal(nominal_value)
