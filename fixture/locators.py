d7e22ffa40082e386539e406f84d5e69c1f63c31
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-24 18:33:36 +0300

    Added one space

--- fixture/locators.py
+++ fixture/locators.py
@@ -5,5 +5,4 @@ class Locators:
 
     CARD_NOMINAL_VALUE_TEXT = (By.CSS_SELECTOR, "h3.par__title.lined-title.lined-title--left")
     NOMINAL_VALUE = (By.CLASS_NAME, "par-options__button")
-    # NOMINAL_500 = (By.XPATH, "(.//*[normalize-space(text()) and normalize-space(.)='Выберите:'])[1]/following::nobr[1]")
     INPUT_RANGE_VALUE = (By.ID, "range-value-input")
