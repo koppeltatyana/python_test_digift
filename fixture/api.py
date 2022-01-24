ba77093dbc61a9cccbb0e1b518bd71b44c88e98d
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-24 19:18:29 +0300

    Реализованы методы get_names_from_response и check_Alcatel_in_names для проверок api-теста

--- fixture/api.py
+++ fixture/api.py
@@ -12,6 +12,19 @@ class ApiHelper:
             return JsTestTask(name=str(json['name']), price=json['price'], image=json['image'])
         return list(map(convert, json))
 
+    def get_names_from_response(self, products_list):
+        names_list = []
+        for product in products_list:
+            names_list += [product.name]
+        return names_list
+
+    def check_Alcatel_in_names(self, names_list):
+        flag = True
+        for name in names_list:
+            if 'Alcatel' not in name:
+                flag = False
+        return flag
+
     def get_js_test_task(self, search_field=None, sort_field=None):
         request_parameters = {'search_field': search_field, 'sort_field': sort_field}
         try:
