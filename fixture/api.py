86d8037348525ef2f60f8cc3c5f6b949cc5641f2
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-24 17:47:56 +0300

    Renamed convert_json_to_model function

--- fixture/api.py
+++ fixture/api.py
@@ -7,18 +7,17 @@ class ApiHelper:
     def __init__(self, api_url):
         self.api_url = api_url
 
-    def convert_json_responce_to_model(self, json_products):
-        def convert(json_products):
-            return JsTestTask(name=str(json_products['name']), price=json_products['price'], image=json_products['image'])
-        return list(map(convert, json_products))
+    def convert_json_to_model(self, json):
+        def convert(json):
+            return JsTestTask(name=str(json['name']), price=json['price'], image=json['image'])
+        return list(map(convert, json))
 
     def get_js_test_task(self, search_field=None, sort_field=None):
         request_parameters = {'search_field': search_field, 'sort_field': sort_field}
         try:
             response = requests.get(self.api_url + 'js-test-task', request_parameters)
             json_response_products = response.json()['products']
-            return self.convert_json_responce_to_model(json_response_products)
-
+            return self.convert_json_to_model(json_response_products)
         except requests.exceptions.HTTPError as err:
             print('Oops. HTTP Error occured')
             print('Response is: {content}'.format(content=err.response.content))
