a35b74061636b31e5dcfcce7bf703f482d6ab942
Tatyana Koppel <tatyana.koppel@mail.ru>
2022-01-25 16:43:52 +0300

    Исправлен метод check_Alcatel_in_names, который теперь не опирается только на название 'Alcatel' в названиях продукта

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,6 +3,5 @@ from model.JsTestTask import JsTestTask
 
 def test_api(api):
     request_result = api.get_js_test_task(search_field="Alcatel", sort_field="name")
-    products_names = api.get_names_from_response(request_result)
     assert request_result == sorted(request_result, key=JsTestTask.sort_key)
-    assert api.check_Alcatel_in_names(products_names)
+    assert api.check_substr_in_names(api.get_names_from_response(request_result), 'Alcatel')
